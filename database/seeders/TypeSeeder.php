053ebe104828e1b903b5f59a69a2d11e640dadb6
n3ndo <ferdinandof3lixbagnale@gmail.com>
2024-02-27 18:41:38 +0100

    [UPDATE] table populated via seeder

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Str;
+
+use App\Models\Type;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 10; $i++) {
+            $newType = new Type();
+            $newType->name = $faker->word();
+            $newType->slug = Str::slug($newType->name, '-');
+            $newType->save();
+        }
+    }
+}
